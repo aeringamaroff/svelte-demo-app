8bfd95773d4c125206717da7b1c29eba618b4f7e
Aerin Gamaroff <aerin@centbee.com>
2024-12-02 11:34:01 +0200

    removed unused styles

--- src/lib/header.svelte
+++ src/lib/header.svelte
@@ -1,11 +1,1 @@
 <header class="navbar text-xl">Expense Tracking App</header>
-
-<style>
-    .layout-header {
-        background-color: #264653;
-        color: #f4a261;
-        font-size: x-large;
-        text-align: center;
-        padding: 0.5em;
-    }
-</style>
